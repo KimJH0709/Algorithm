188d2ef07b0b420f336c964a8683a347fac06440
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-04-08 20:50:51 +0900

    [Bronze I] Title: 세로읽기, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = new String[5];
+		for(int i = 0; i < 5; i++) {
+			str[i] = br.readLine();
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < 15; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(str[j].length() > i) {
+					sb.append(str[j].charAt(i));
+				} else {
+					continue;
+				}
+			}
+		}
+		
+		System.out.println(sb);
+		
+		br.close();
+
+	}
+
+}
