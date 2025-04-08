a525b3feb32c087f3736138126a03fe8457af760
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-04-08 20:42:39 +0900

    [Silver IV] Title: 문자열 집합, Time: 288 ms, Memory: 38980 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,31 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int n, m, count;
+    static Set<String> set;
+
     public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        set = new HashSet<>();
 
-        Set<String> set = new HashSet<>();
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             set.add(br.readLine());
         }
 
-        int count = 0;
-        for (int i = 0; i < M; i++) {
+        count = 0;
+        for (int i = 0; i < m; i++) {
             if (set.contains(br.readLine())) {
                 count++;
             }
         }
 
         System.out.println(count);
-
     }
 }
