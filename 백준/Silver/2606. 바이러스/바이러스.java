fa9f67657727a23ea30b52a9c4e2e2576da08a23
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-04-12 22:00:51 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11744 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,39 +1,61 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Main {
-    static int N, M;
-    static int[][] graph;
-    static boolean[] visited;
-    static int cnt = 0;
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
-
-        graph = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
-
-        for (int i = 0; i < M; i++) {
-            int src = scanner.nextInt();
-            int dest = scanner.nextInt();
-            graph[src][dest] = 1;
-            graph[dest][src] = 1;
-        }
-
-        dfs(1);
-
-        System.out.println(cnt - 1);
-
-
-    }
-
-    static void dfs(int node) {
-        visited[node] = true;
-        cnt++;
-        for (int i = 1; i <= N; i++) {
-            if (graph[node][i] == 1 && !visited[i]) {
-                dfs(i);
-            }
-        }
-    }
+	
+	static boolean[] visited;
+	static LinkedList<Integer>[] list;
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws Exception {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+		int m = Integer.parseInt(br.readLine()); // 간선의 수
+		
+		visited = new boolean[n + 1];
+		list = new LinkedList[n + 1];
+		for (int i = 1; i <= n; i++) {
+			list[i] = new LinkedList<>();
+		}
+		
+		for (int i = 0; i < m ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int src = Integer.parseInt(st.nextToken());
+			int dst = Integer.parseInt(st.nextToken());
+			
+			list[src].add(dst);
+			list[dst].add(src);
+		}
+		
+		bfs(1);
+		
+		int count = 0;
+		for (int i = 2; i <= n; i++) {
+			if (visited[i])	count++;
+		}
+		
+		System.out.println(count);
+		
+	}
+	private static void bfs(int start) {
+	
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		visited[start] = true;
+		
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			
+			for (int next : list[now]) {
+				if (!visited[next]) {
+					visited[next] = true;
+					q.add(next);
+				}
+			}
+		}
+		
+	}
 }
