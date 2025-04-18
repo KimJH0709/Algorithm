647fa55e42fe3e7ec64535db90f562dfeb182289
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-04-18 17:05:28 +0900

    [level 2] Title: 올바른 괄호, Time: 20.41 ms, Memory: 55.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty())    return false;
+                if (ch == ')' && stack.pop() != '(')    return false;
+                if (ch == '}' && stack.pop() != '{')    return false;
+                if (ch == ']' && stack.pop() != '[')    return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
