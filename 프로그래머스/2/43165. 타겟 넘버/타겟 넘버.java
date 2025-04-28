bda7845aced24ea95a55a2cb632dd639377d17eb
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-04-28 10:23:50 +0900

    [level 2] Title: 타겟 넘버, Time: 5.14 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,18 +2,18 @@ class Solution {
     static int answer = 0;
     public int solution(int[] numbers, int target) { 
         
-        bfs(0, 0, numbers, target);
+        dfs(0, 0, numbers, target);
         return answer;
     }
     
-    static void bfs(int sum, int index, int[]numbers, int target) {
+    static void dfs(int sum, int index, int[]numbers, int target) {
         if (index == numbers.length) {
             if (sum == target)  answer++;
         }        
         
         if (index < numbers.length) {
-            bfs(sum + numbers[index], index + 1, numbers, target);
-            bfs(sum - numbers[index], index + 1, numbers, target);    
+            dfs(sum + numbers[index], index + 1, numbers, target);
+            dfs(sum - numbers[index], index + 1, numbers, target);    
         }
     }
 }
