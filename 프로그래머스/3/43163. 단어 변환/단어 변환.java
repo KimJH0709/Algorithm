2a23145ca613aafef7ae3bde6ed04d2258e27902
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-05-26 16:30:02 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,54 +1,51 @@
 import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
         
-        boolean isContain = false;
-        for (String word : words) {
-            if (word.equals(target))
-                isContain = true;
-        }
-        if (!isContain) return 0;
-        
-        Queue<Integer> q = new LinkedList<>();
+        Queue<String> q = new ArrayDeque<>();
         int[] visited = new int[words.length];
+        int count = 0;
         
         for (int i = 0; i < words.length; i++) {
-            if (diffOne(begin, words[i])) {
-                q.add(i);
+            if (isDiffOne(begin, words[i])) {
+                q.add(words[i]);
                 visited[i] = 1;
             }
         }
         
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            String nowWord = words[now];
-
+        while(!q.isEmpty()) {
+            String now = q.poll();
+            
+            int now_idx = -1;
             for (int i = 0; i < words.length; i++) {
-                if (visited[i] == 0 && diffOne(nowWord, words[i])) {
-                    visited[i] = visited[now] + 1;
-                    q.add(i);
+                if (now == words[i])
+                    now_idx = i;
+            }
+            
+            if (now.equals(target)) {
+                return visited[now_idx];
+            }
+            
+            for (int i = 0; i < words.length; i++) {
+                if (isDiffOne(now, words[i]) && visited[i] == 0) {
+                    q.add(words[i]);
+                    visited[i] = visited[now_idx] + 1;
                 }
             }
         }
         
-         for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(target)) {
-                return visited[i];
-            }
-        }
-
         return 0;
-        
     }
     
-    static boolean diffOne(String str1, String str2) {
+    public boolean isDiffOne(String begin, String target) {
         int count = 0;
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) != str2.charAt(i))
+        
+        for (int i = 0; i < begin.length(); i++) {
+            if (begin.charAt(i) != target.charAt(i))
                 count++;
         }
         
-        return count == 1 ? true : false;
-    }
+        return count == 1;
+    } 
 }
