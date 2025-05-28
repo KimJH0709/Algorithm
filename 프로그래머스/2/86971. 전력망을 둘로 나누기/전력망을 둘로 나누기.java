985346b24c408c802b3ea720eeb12e8acd31579f
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-05-28 15:17:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 16.07 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,57 +1,53 @@
 import java.util.*;
-
 class Solution {
-    static List<Integer>[] list;
-    
     public int solution(int n, int[][] wires) {
-        
         int answer = Integer.MAX_VALUE;
-        
-        list = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
+        List<Integer>[] list = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
             list[i] = new ArrayList<>();
-        }
-
-        for (int[] wire : wires) {
-            list[wire[0]].add(wire[1]);
-            list[wire[1]].add(wire[0]);
         }
         
         for (int[] wire : wires) {
             int src = wire[0];
             int dst = wire[1];
-
-            list[src].remove(Integer.valueOf(dst));
+            
+            list[src].add(dst);
+            list[dst].add(src);
+        }
+        
+        for (int[] wire : wires) {
+            int src = wire[0];
+            int dst = wire[1];
+            
             list[dst].remove(Integer.valueOf(src));
-
-            int rs = Math.abs(n - 2 * bfs(src, n));
-            answer = Math.min(answer, rs);
-
+            list[src].remove(Integer.valueOf(dst));
+            
+            Queue<Integer> q = new ArrayDeque<>();
+            boolean[] visited = new boolean[n + 1];
+            
+            q.add(1);
+            visited[1] = true;
+            
+            int count = 1;
+            
+            while(!q.isEmpty()) {
+                int now = q.poll();
+                
+                for (int next : list[now]) {
+                    if (!visited[next]) {
+                        q.add(next);
+                        visited[next] = true;
+                        count++;
+                    }
+                }
+            }
+            
+            answer = Math.min(answer, Math.abs(n - count - count));
+            
             list[src].add(dst);
             list[dst].add(src);
         }
         
         return answer;
-    }
-    
-    static int bfs(int start, int n) {
-        boolean[] visited = new boolean[n + 1];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = true;
-        int count = 1;
-
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            for (int next : list[now]) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    q.add(next);
-                    count++;
-                }
-            }
-        }
-
-        return count;
     }
 }
