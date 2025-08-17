c09e5d14a69f252e4792aeb446dca07ac00cb3bf
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-08-17 20:24:21 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -7,21 +7,15 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int count2 = 0;
         int count5 = 0;
         for (int i = 2; i <= n; i++) {
             int temp = i;
-            while (temp % 2 == 0) {
-                count2++;
-                temp /= 2;
-            }
-
             while (temp % 5 == 0) {
                 count5++;
                 temp /= 5;
             }
         }
 
-        System.out.println(Math.min(count2, count5));
+        System.out.println(count5);
     }
 }
