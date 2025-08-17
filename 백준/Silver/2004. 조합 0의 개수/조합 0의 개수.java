b37ac7829c50538efb66192f3b3de1c119f689fd
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-08-17 20:41:36 +0900

    [Silver II] Title: 조합 0의 개수, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        long count2 = getCount(n, 2) - getCount(m, 2) - getCount(n - m, 2);
+        long count5 = getCount(n, 5) - getCount(m, 5) - getCount(n - m, 5);
+        System.out.println(Math.min(count2, count5));
+    }
+
+    static long getCount(long num, long divNum) {
+        long count = 0;
+        while (num > 0) {
+            num /= divNum;
+            count += num;
+        }
+        return count;
+    }
+}
