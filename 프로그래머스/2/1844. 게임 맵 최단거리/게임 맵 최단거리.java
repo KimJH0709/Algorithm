d6183aaa102f6454be978c6d405b1e005a8a635c
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-09-03 01:04:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.10 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,16 +1,20 @@
 import java.util.*;
-
 class Solution {
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static int[][] visited;
+    
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
     public int solution(int[][] maps) {
         
         int n = maps.length;
         int m = maps[0].length;
         
-        Queue<Point> q = new LinkedList<>();
-        visited = new int[n][m];
+        Queue<Point> q = new ArrayDeque<>();
+        int[][] visited = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visited[i], -1);
+        }
+        
+        if (maps[0][0] == 0)    return -1;
         
         q.add(new Point(0, 0));
         visited[0][0] = 1;
@@ -22,16 +26,14 @@ class Solution {
                 int nr = now.r + dr[d];
                 int nc = now.c + dc[d];
                 
-                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                if (visited[nr][nc] != 0 || maps[nr][nc] == 0) continue;
-                
-                q.add(new Point(nr, nc));
-                visited[nr][nc] = visited[now.r][now.c] + 1;
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && visited[nr][nc] == -1 && maps[nr][nc] == 1) {
+                    q.add(new Point(nr, nc));
+                    visited[nr][nc] = visited[now.r][now.c] + 1;
+                }
             }
         }
         
-        int answer = visited[n - 1][m - 1];
-        return answer == 0 ? -1 : answer;
+        return visited[n - 1][m - 1];
     }
 }
 
@@ -42,4 +44,3 @@ class Point {
         this.c = c;
     }
 }
-
