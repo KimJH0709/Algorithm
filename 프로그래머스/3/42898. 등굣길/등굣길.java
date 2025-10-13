6b2efe5a2acd93ed941f6803486e1466ff6478fc
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-10-14 04:35:25 +0900

    [level 3] Title: 등굣길, Time: 2.29 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -8,7 +8,7 @@ class Solution {
             for (int j = 1; j < m + 1; j++) {
                 if (i == 1 && j == 1) continue;
                 
-                if (inWater(i, j, puddles))
+                if (water(i, j, puddles))
                     dp[i][j] = 0;
                 else
                     dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
@@ -18,7 +18,7 @@ class Solution {
         return dp[n][m];
     }
     
-    public boolean inWater (int i, int j, int[][] puddles) {
+    public boolean water (int i, int j, int[][] puddles) {
         for (int[] pd : puddles) {
             if (pd[0] == j && pd[1] == i)
                 return true;
