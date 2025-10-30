fd2df0117c573f30a920e3828e7bb7a50fae2043
KIMJAEHYEON <117272556+KimJH0709@users.noreply.github.com>
2025-10-30 22:34:45 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 584 ms, Memory: 93972 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,65 +1,73 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
     static int n, m;
-    static int[][] board;
-    static int[][][] visited;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        board = new int[n + 1][m + 1];
-        visited = new int[n + 1][m + 1][2];
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= n; i++) {
-            String line = sc.next();
-            for (int j = 1; j <= m; j++) {
-                board[i][j] = line.charAt(j - 1) - '0';
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = line.charAt(j) - '0';
             }
         }
 
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(1, 1, 0));
-        visited[1][1][0] = 1;
+
+        Queue<Point> q = new ArrayDeque<>();
+        int[][][] visited = new int[n][m][2];
+
+        q.add(new Point(0, 0, 0));
+        visited[0][0][0] = 1;
 
         while (!q.isEmpty()) {
             Point now = q.poll();
-            if (now.r == n && now.c == m) {
-                System.out.println(visited[now.r][now.c][now.isBroken]);
-                return ;
+
+            if (now.r == n - 1 && now.c == m - 1) {
+                System.out.println(visited[now.r][now.c][now.canBreak]);
+                return;
             }
 
-            for (int i = 0; i < 4; i++) {
-                 int nr = now.r + dr[i];
-                 int nc = now.c + dc[i];
+            for (int d = 0; d < 4; d++) {
+                int nr = now.r + dr[d];
+                int nc = now.c + dc[d];
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
 
-                 if (nr <= 0 || nr > n || nc <= 0 || nc > m) continue;
-                 if (visited[nr][nc][now.isBroken] == 0) {
-                    if (board[nr][nc] == 0) {
-                        visited[nr][nc][now.isBroken] = visited[now.r][now.c][now.isBroken] + 1;
-                        q.add(new Point(nr, nc, now.isBroken));
-                    } else if (board[nr][nc] == 1 && now.isBroken == 0) {
+                if (map[nr][nc] == 0) {
+                    if (visited[nr][nc][now.canBreak] == 0) {
+                        visited[nr][nc][now.canBreak] = visited[now.r][now.c][now.canBreak] + 1;
+                        q.add(new Point(nr, nc, now.canBreak));
+                    }
+                } else {
+                    if (now.canBreak == 0 && visited[nr][nc][1] == 0) {
                         visited[nr][nc][1] = visited[now.r][now.c][0] + 1;
                         q.add(new Point(nr, nc, 1));
                     }
                 }
             }
         }
+
         System.out.println(-1);
+        return;
     }
 }
 
 class Point {
-    int r, c;
-    int isBroken;
-    Point(int r, int c, int isBroken) {
+    int r, c, canBreak;
+
+    Point(int r, int c, int canBreak) {
         this.r = r;
         this.c = c;
-        this.isBroken = isBroken;
+        this.canBreak = canBreak;
     }
 }
